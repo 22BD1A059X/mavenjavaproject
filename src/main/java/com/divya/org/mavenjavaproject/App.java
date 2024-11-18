b4a4c0e5885e93a943bf893a765eaad02db1e1b1
Divya <Divya>
2024-11-18 12:08:17 +0530

    fisrt change

---
+++ src/main/java/com/divya/org/mavenjavaproject/App.java
@@ -0,0 +1,13 @@
+package com.divya.org.mavenjavaproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
